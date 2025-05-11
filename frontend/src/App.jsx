f62f7310130a1b882544c2e87cb9fded3fa28373
abdurehmanSualih <abdurehmansualih00@gmail.com>
2025-05-11 17:53:07 +0300

    complate structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { MdDelete } from "react-icons/md";
+import { MdEdit } from "react-icons/md";
+import { IoClose } from "react-icons/io5";
+import { MdOutlineDone } from "react-icons/md";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [editing, setEditing] = useState(true);
   return (
-    <>
+    <div>
+      <header>TODO LIST</header>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <input type="text" />
+        <button>ADD</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <ul>
+          <li>
+            {!editing ? (
+              <>
+                <input type="checkbox" />
+                <span>todos </span>
+              </>
+            ) : (
+              <input type="text" />
+            )}
+            {!editing ? (
+              <>
+                <button>
+                  <MdEdit />
+                </button>
+                <button>
+                  <MdDelete />
+                </button>
+              </>
+            ) : (
+              <>
+                <button>
+                  <MdOutlineDone />
+                </button>
+                <button>
+                  <IoClose />
+                </button>
+              </>
+            )}
+          </li>
+        </ul>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
